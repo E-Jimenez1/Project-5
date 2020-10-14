93da8acfb0dd3432108208e4ca41adbcb9e3e2c1
E-Jimenez1 <64759027+E-Jimenez1@users.noreply.github.com>
2020-10-14 00:53:59 -0700

    Delete filterSearch.js

--- Project 5/dist/js/filterSearch.js
+++
@@ -1,6 +0,0 @@
- const list = document.querySelector('.gallery a');
- const searchBar = document.forms ['search'].querySelector('input');
- searchBar.addEventListener('keyup', function(e) {
-    const term = e.target.value.toLowerCase();
-    const captions = list.getElementsByTagName('img');
- });
